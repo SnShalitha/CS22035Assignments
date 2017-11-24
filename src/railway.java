85fd9b00426cbad77ff395677a9b5563d5876c5f
Cyril Mac <thari69@gmail.com>
2017-11-24 11:17:58 +0530

    Version 1 - November 24th

--- src/railway.java
+++ src/railway.java
@@ -3,4 +3,12 @@ public class railway {
     public int tripCount;
     public float totalIncomeTrips;
     public float expenses;
+
+    railway(String inName)
+    {
+        this.railwayName = inName;
+        tripCount = 0;
+        totalIncomeTrips = 0;
+        expenses = 0;
+    }
 }
--- src/train.java
+++ src/train.java
@@ -2,4 +2,11 @@ public class train {
     public String trainID;
     public int numberOfPassengers;
     public String model;
+
+    train(String inTID,int inNumPassengers,String inModel)
+    {
+        this.trainID = inTID;
+        this.numberOfPassengers = inNumPassengers;
+        this.model = inModel;
+    }
 }
